e6c690833b6f3c6b4f67ed7a93fe3db03ec7dfd5
dhuy93 <d.huy93@gmail.com>
2014-04-30 17:56:43 +0700

    Xử lý chuột
    Xử lý chuột xong rồi,
    tạo databasse của mấy cái English word
    Thêm mấy biến global trong main.cpp
    
    Đang mò cách vẽ hết tất cả chữ cái trong 1 word, chắc phải truyền thêm
    cái parameter là base coordinate cho mấy hàm vẽ để khi vẽ thì nó sẽ cộng
    thêm mấy cái base coordinate đó rồi mối vẽ (cho khỏi bị chữ này đè lên
    chữ kia)

--- Project2D/Project2D/main.cpp
+++ Project2D/Project2D/main.cpp
@@ -1,11 +1,32 @@
 #include"Line.h"
 #include "Letter.h"
+#include <time.h>		/* time */
+#include <stdlib.h>		/* srand, rand */
 
-int wWidth = 400;	// width of window
-int wHeight = 300;	// height of window
+
+// Global vars
+int x_1, x_2;
+int y_1, y_2;
+Line globalLine;
+const int SIZE_OF_DICTIONARY = 5;
+// Dictionary
+string dictionary[SIZE_OF_DICTIONARY] = { 
+	"Graphics", 
+	"Computer", 
+	"DucHuy", 
+	"PhucKhanh",
+	"Project"
+};
+
+
+
+int wWidth = 1024;	// width of window
+int wHeight = 768;	// height of window
 
 void RenderScene(void);
 void SetupRC(void);
+void MouseClick(int iButton, int iState, int x, int y);	// Callback function triggered when mouse button is actived
+void MouseMotionFunction(int x, int y);					// Callback function triggered when mouse moves when one mouse button is pressed
 
 
 int main(int argc, char* argv[]) {
@@ -16,6 +37,10 @@ int main(int argc, char* argv[]) {
 
 	glutDisplayFunc(RenderScene);
 	SetupRC();
+
+	// Register callback functions for mouse control
+	glutMouseFunc(MouseClick);
+	glutMotionFunc(MouseMotionFunction);
 	glutMainLoop();
 
 	
@@ -42,12 +67,46 @@ void RenderScene(void) {
 	glFlush();
 
 
+	// TODO: Display a random word from dictionary
+	/*int index = rand() % SIZE_OF_DICTIONARY;
+	string word = dictionary[index];
+	for (int i = 0; i < word.length(); ++i) {
+
+	}*/
+
 	Z a;
 	a.Draw();
-	a.fillColor();
+	//a.fillColor();
 }
 
 void SetupRC(void) {
 	glClearColor(0.5, 0.5, 0.5, 1);
 }
 
+
+void MouseClick(int iButton, int iState, int x, int y) {
+	if (iButton == GLUT_LEFT_BUTTON) {
+		// if the button is the left button
+		if (iState == GLUT_DOWN) {
+			// if left button is clicked
+			x_1 = x;
+			y_1 = wHeight - y;
+		}
+		else if (iState == GLUT_UP) {
+			// if left button is released
+			x_2 = x;
+			y_2 = wHeight - y;
+			globalLine.setEndPoints(Vec2f(x_1, y_1), Vec2f(x_2, y_2));
+			globalLine.Draw();
+		}
+		
+	}
+}
+
+void MouseMotionFunction(int x, int y) {
+	x_2 = x;
+	y_2 = wHeight - y;
+	globalLine.setEndPoints(Vec2f(x_1, y_1), Vec2f(x_2, y_2));
+	RenderScene();
+	globalLine.Draw();
+}
